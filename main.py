fbbd7b7ab386945a1752d6c3a7332847bfac9575
Bhagat Aastha <109658@eesd.org>
2021-01-15 00:26:04 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,70 @@
+'''
+01/14/2021
+C1: Input
+
+Review: 
+How do you get Output??
+>> print ("Hello World")
+OUTPUT: Hello World
+
+Input
+The way the compurter gets information from the user
+
+Python initially treats inputs as a string type
+
+Ex.
+5 --> "5"
+hi --> "hi"
+
+Fromula Creating a variable:
+  variableName = input(PROMPT)
+
+  '''
+
+# name = "Aastha"
+#print ("My name is " + str(name))
+
+#name1 = input()
+#print("My name is " + name1)
+
+
+
+# Ask user for school
+# Ask user for grade
+# Ask user for favorite color
+# Ask user for the year they were born
+# Ask user for age
+# Print them all out
+
+#school = input("What is your school")
+#school = " quimby oak"
+#print("the school is" + school)
+
+
+grade = input("What is your grade")
+#print("the grade is " + grade)
+
+color = input("What is your favorite color")
+#print("the color is" + color)
+
+
+year = input("What year were you born on")
+#print("the year is" + year)
+
+
+age = input("What age are you")
+#print("the age is" + age)
+
+print("the grade is " + grade)
+print("the color is" + color)
+print("the year is" + year)
+print("the age is" + age)
+
+
+
+
+
+
+
+
+
